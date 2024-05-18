686e2e3def4b7d79ed98259fefdbae12a1942552
josebuib <josebuitragob75@gmail.com>
2024-05-17 20:46:34 -0500

    Clase CRUD 170524

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
 /* -------------------------- import dependencias ------------------------- */
 import express from 'express';
+import morgan from 'morgan';
+
+import {join, dirname} from 'path'
+import {fileURLToPath} from 'url'
+import {engine} from 'express-handlebars'
+
+
 /* ----------------------------- initialization ----------------------------- */
 const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
 /* --------------------------------- setting -------------------------------- */
 app.set('port', process.env.PORT || 3000 );
+
+app.set('views', join(__dirname,'views'));
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'),'layouts'), 
+    partialsDir: join(app.get('views'),'partials'),
+    extname: '.hbs'
+}));
+
+app.set('view engine','.hbs');
+
 /* ------------------------------- middlewares ------------------------------ */
 
-/* --------------------------------- routes --------------------------------- */
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+/* --------------------------------- routes --------------------------------- */
+app.get('/', (req, res) => {
+    res.json({"message": "Hola Mundo"})
+});
 /* ------------------------------ Public files ------------------------------ */
+
+app.use(express.static(join(__dirname, 'public')));
 
 /* ------------------------------- run server ------------------------------- */
 app.listen(app.get('port'), ()=>{
