f5ee3f0e341dd892c921a990210b3dc3afcd0fc1
josebuib <josebuitragob75@gmail.com>
2024-05-20 19:48:20 -0500

    Inicio pagina index

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.use(express.json());
 
 /* --------------------------------- routes --------------------------------- */
 app.get('/', (req, res) => {
-    res.json({"message": "Hola Mundo"})
+    res.render('index')
 });
 /* ------------------------------ Public files ------------------------------ */
 
