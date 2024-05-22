1a0b2b1f758f76e540b99f252830dd76ad84c612
josebuib <josebuitragob75@gmail.com>
2024-05-21 20:29:46 -0500

    interaccion Base de Datos

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import morgan from 'morgan';
 import {join, dirname} from 'path'
 import {fileURLToPath} from 'url'
 import {engine} from 'express-handlebars'
+import personasRoutes from './routes/personas.routes.js'
+
 
 
 /* ----------------------------- initialization ----------------------------- */
@@ -36,6 +38,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.render('index')
 });
+
+app.use(personasRoutes);
 /* ------------------------------ Public files ------------------------------ */
 
 app.use(express.static(join(__dirname, 'public')));
