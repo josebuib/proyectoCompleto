212a6912149b74783ec4df1ac019b264c852ef0f
josebuib <josebuitragob75@gmail.com>
2024-05-22 18:47:28 -0500

    Crear funcion eliminar datos

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -3,17 +3,24 @@ import pool from '../database.js'
 
 const router = Router();
 
-
 router.get('/list', async(req, res) => {
     try {
         const [result] = await pool.query('SELECT * FROM personas');
         res.render('personas/list', {personas: result})
-        console.log(result);
+       } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+router.get('/delete/:id', async(req, res)=>{
+    try {
+
+        const {id} = req.params
+        await pool.query('DELETE FROM personas WHERE id = ?', [id]);
+        res.redirect('/list')
     } catch (error) {
         res.status(500).json({message: error.message});
-
-    }
-
+    }   
 });
 
 export default router;
